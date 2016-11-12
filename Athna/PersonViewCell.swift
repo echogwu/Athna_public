843fe54cf46c1c83c1b9df80c3528eb4ee240048
WuGuihua <Echo@dhcp-138.festnet>
2016-11-11 20:55:09 -0800

    show people on home tab, have not added the feature of show people's profiles yet

---
+++ Athna/PersonViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PersonViewCell.swift
+//  Athna
+//
+//  Created by WuGuihua on 11/11/16.
+//  Copyright © 2016 Sherlock. All rights reserved.
+//
+
+import UIKit
+
+class PersonViewCell: UICollectionViewCell {
+    
+}
