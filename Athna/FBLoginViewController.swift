31173e65eb4e87317318cee4ea3f869d761002e3
WuGuihua <Echo@dhcp-138.festnet>
2016-11-30 13:08:45 -0800

    implemented facebook login module, have not integrated it into the project yet

---
+++ Athna/FBLoginViewController.swift
@@ -0,0 +1,54 @@
+//
+//  FBLoginViewController.swift
+//  Athna
+//
+//  Created by WuGuihua on 11/29/16.
+//  Copyright © 2016 Sherlock. All rights reserved.
+//
+
+import UIKit
+import FBSDKLoginKit
+
+class FBLoginViewController: UIViewController, FBSDKLoginButtonDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        let loginView : FBSDKLoginButton = FBSDKLoginButton()
+        self.view.addSubview(loginView)
+        loginView.center = self.view.center
+        loginView.readPermissions = ["public_profile", "email", "user_friends"]
+        
+        loginView.delegate = self
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
+        
+    }
+    
+    func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
+        
+    }
+    
+    func loginButtonWillLogin(_ loginButton: FBSDKLoginButton!) -> Bool {
+        return true
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
