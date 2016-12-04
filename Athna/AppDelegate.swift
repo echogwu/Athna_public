a930582fb6a3f83e1de7573672b5c7473fa0945b
WuGuihua <Echo@WuGuihuas-MacBook-Air.local>
2016-12-03 17:11:17 -0800

    add AWS sdks and sign in files

--- Athna/AppDelegate.swift
+++ Athna/AppDelegate.swift
@@ -17,8 +17,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
-        return true
+        //FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
+        //return true
+        return AWSMobileClient.sharedInstance.didFinishLaunching(application: application, withOptions: launchOptions as [NSObject : AnyObject]?)
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
@@ -37,15 +38,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-        FBSDKAppEvents.activateApp()
+        //FBSDKAppEvents.activateApp()
+        AWSMobileClient.sharedInstance.applicationDidBecomeActive(application: application)
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
+    
     func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
-        return FBSDKApplicationDelegate.sharedInstance().application(app, open: url, sourceApplication: options[UIApplicationOpenURLOptionsKey.sourceApplication] as! String, annotation: options[UIApplicationOpenURLOptionsKey.annotation])
+        //return FBSDKApplicationDelegate.sharedInstance().application(app, open: url, sourceApplication: options[UIApplicationOpenURLOptionsKey.sourceApplication] as! String, annotation: options[UIApplicationOpenURLOptionsKey.annotation])
+        return AWSMobileClient.sharedInstance.withApplication(application: app, withURL: url as NSURL, withSourceApplication: options[UIApplicationOpenURLOptionsKey.sourceApplication] as! String?, withAnnotation: options[UIApplicationOpenURLOptionsKey.annotation] as AnyObject)
     }
     
     
